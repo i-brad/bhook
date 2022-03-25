7fd15e9b9f238e003d64fd87ff401f8f49639855
MickelsonKlaus <klausmickelson52@email.com>
2022-03-25 20:50:52 +0100

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,7 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+getAnalytics(app)
 const auth = getAuth(app)
 const db = getFirestore(app)
 const storage = getStorage(app)
