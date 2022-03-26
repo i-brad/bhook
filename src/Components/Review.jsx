9f2deabf5788005c237cca258eb1b7f5355d544c
MickelsonKlaus <84320904+MickelsonKlaus@users.noreply.github.com>
2022-03-26 20:41:16 +0100

    Update Review.jsx

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -1,8 +1,8 @@
 function Review({ data }) {
   return (
     <blockquote className="block w-full max-w-full text-accent bg-green-100 rounded-md p-3">
-      <q className="text-lg block mb-3">{data?.review}</q>
-      <cite className="text-sm block text-right">
+      <q className="text-sm md:text-lg block mb-3">{data?.review}</q>
+      <cite className="text-xs md:text-sm block text-right">
         {data?.name || "anonymous"}
       </cite>
     </blockquote>
