fb1dbf7218d38ddd685cb9288dd7b0b567541adc
MickelsonKlaus <84320904+MickelsonKlaus@users.noreply.github.com>
2022-03-26 20:53:55 +0100

    Update Book.jsx

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -250,7 +250,7 @@ setTimeout(() => {
                 disabled={sending}
                 className={`${sending? "opacity-50" : "opacity-100"} flex justify-between items-center bg-accent rounded-3xl text-white px-4 py-2 mt-3 mx-auto`}
               >
-                <RateReviewOutlinedIcon className="mr-1" /> Send
+                <RateReviewOutlinedIcon className="mr-1" />{sending? "Sending" : "Send"}
               </button>
             </Form>
           )}
